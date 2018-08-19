4b4da0594eae0b0b6cba4ca2faedef04effb262f
unknown <abolfathi.al@gmail.com>
2018-08-19 14:17:45 +0430

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,30 @@
+const {MongoClient, ObjectId} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+  
+  if(err)
+    return console.log('Unable to connect MongoDB server');
+
+  console.log('Connect to MongoDB server');
+
+  const db = client.db('TodoApp');
+  
+  // db.collection('Todes').insertOne({
+  //   text: 'Someting to do',
+  //   completed: false,
+  //   brithDate: new Date(2006,10,30)
+  // }, (err, result) => {
+  //   if(err)
+  //     return console.log('Unable to insert todo', err);
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Todes').find().toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos',err);
+  });
+  //client.close();
+});
