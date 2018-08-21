a43942bc94ed7ca28727b5793002d598946b61ab
unknown <abolfathi.al@gmail.com>
2018-08-21 08:48:35 +0430

    add hotel method

--- server/server.js
+++ server/server.js
@@ -1,18 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {
-  mongoose
-} = require('./db/mongoose');
-var {
-  Todo
-} = require('./models/todo');
-var {
-  User
-} = require('./models/user');
-var {
-  Hotel
-} = require('./models/hotel');
+var { ObjectID } = require('mongodb');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+var { Hotel } = require('./models/hotel');
 
 
 var app = express();
@@ -40,6 +33,22 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  
+  if(!ObjectID.isValid(id))
+    return res.status(404).send();
+
+  Todo.findById(id).then((todo) => {
+    if(!todo)
+      return res.status(404).send();
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.get('/hotel', (req, res) => {
